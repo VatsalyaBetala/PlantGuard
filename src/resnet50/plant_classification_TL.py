ab42372f3d875838356f6987f0b0bf5a1815c61a
Vatsalya Betala <vatsalyabetala@gmail.com>
2025-02-19 15:10:30 +0530

    Add ResNet50 transfer learning pipeline with preprocessing, classification, and training scripts

---
+++ src/resnet50/plant_classification_TL.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class PlantClassifierCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(PlantClassifierCNN, self).__init__()
+
+        # Load ResNet50 without downloading
+        self.model = models.resnet50(weights=None)  # Do not download
+        weight_path = "/data_home/vatsalya/PlantDiseaseClassification/Plant_Classification/src/TranferLearning/resnet50_weights.pth"
+
+        # Load manually downloaded weights
+        self.model.load_state_dict(torch.load(weight_path, map_location=torch.device("cuda" if torch.cuda.is_available() else "cpu")))
+
+        # Freeze all layers except the last classification layer
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # Replace the final classification layer
+        in_features = self.model.fc.in_features
+        self.model.fc = nn.Sequential(
+            nn.Linear(in_features, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        return self.model(x)
