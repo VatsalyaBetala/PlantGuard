d2b07f788124a340985db01a0ee778b60fa5e057
Vatsalya Betala <150043725+VatsalyaBetala@users.noreply.github.com>
2025-06-12 14:52:03 +0530

    Added ground level understanding of GRAD-CAM
    - Added extensive sequential working of GRAD-CAM.
    - Intuitive dimensional understanding of the working.

--- src/Disease_Classification_resnet50/src/grad_cam.py
+++ src/Disease_Classification_resnet50/src/grad_cam.py
@@ -7,6 +7,30 @@ import cv2
 import matplotlib.pyplot as plt
 from disease_model import DiseaseClassifier
 
+
+
+# The underlying understanding of how GradCams works lies the understanding of the gradients: 
+
+# NOTE:GradCams looks at the last convolution layer, just before the fully-connected layer(s) (the last Conv-layer is unfreezed to capture gradients). 
+
+# STEP1: Firstly, the feature maps are calculated by convolving each filter (in this case 2048) over the output of the previous layer.
+
+# STEP2: This leaves you with this activations of dimension : [1,2048,7,7]. (2048 filters, each outputting 7x7 feature map)
+
+# STEP3: Then, for each filter map is AveragePooled, which leaves you with one value for each feature map: [1,2048]. 
+
+# STEP4: We calculte the weighted sum of feature maps. Following is the dimensional understanding: 
+# activation[i]      |  [7, 7]  | The i-th feature map A^i from layer4      |
+# w                  |  scalar  | a(i), the importance of that feature map  |
+# w * activation[i]  |  [7, 7]  | Scaled feature map                        |
+# cam += ...         |  [7, 7]  | Running sum of all weighted feature maps  |
+
+# RESULT: A [1,7,7], which serves as the GRAD-CAM heatmap, where each pixel tells you how important that spatial location was for the predicted class
+
+# POSTPROCESSING: Finally, we apply ReLU, resize it, and normalize it.
+
+# We apply a visual overlay on the original image.    
+
 IMG_PATH = r'C:\Users\Vatsalya Betala\OneDrive\Documents\Repositories\plant-disease-classification\src\Disease_Classification_resnet50\src\Late_Blight_1.jpg'
 MODEL_PATH = r'C:\Users\Vatsalya Betala\OneDrive\Documents\Repositories\plant-disease-classification\src\models\Potato_Disease_Classification.pth'
 NUM_CLASSES = 3  
